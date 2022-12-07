de282530581ee8aad4300d260d7cc0b6d1b8fb01
Stephen Berg <sqb6128@psu.edu>
2022-12-06 22:06:22 -0500

    start rebuilding site

--- static/presentations/materials/addUp.R
+++
@@ -1,7 +0,0 @@
-            addUp_R<-function(n){
-                val=0
-                for (i in 1:n){
-                    val=val+i
-                }
-                return(val)
-            }
